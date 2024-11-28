a75a2a602b215c0797b8b8ff1f417bee08832340
PabloGalan13 <159865499+PabloGalan13@users.noreply.github.com>
2024-11-28 00:01:54 -0700

    oa
    oa

--- MatchMaker_PabloGalan/scripts/formActions.js
+++ MatchMaker_PabloGalan/scripts/formActions.js
@@ -36,6 +36,8 @@ window.onload = function () {
         select.addEventListener('blur', noResaltar);
     }
     llenarNacionalidad();
+    /*form[0].onsubmit=enviar;*/
+    form[0].onsubmit=enviar2;
 }
 
 function llenarNacionalidad() {
@@ -61,5 +63,62 @@ function noResaltar(evento) {
 
 function resaltarDesresaltar(evento) {
     evento.target.classList.toggle("selected");
+}
 
+function obtenerGenero() {
+    /*
+    const gSelected= document.querySelectorAll("input[name=interest]:checked");
+    for (let g of gSelected) {
+        gactivos.push(g.value);
+    }
+    */
+
+    const genero = document.getElementsByName("interest");
+    const gactivos = [];
+    for (let g of genero) {
+        if (g.checked) {
+            gactivos.push(g.value);
+        }
+    }
+    if (gactivos.length === 0) {
+        gactivos.push("male");
+        gactivos.push("female");
+    }
+    const index = gactivos.length > 1 ? Math.floor(Math.random() * 2) : 0;
+    return gactivos[index];
+}
+
+function enviar(evento) {
+    evento.preventDefault();
+    const nacionalidad = document.getElementById("nationality");
+    const genero = obtenerGenero();
+    window.location.href = `its-a-match.html?nac=${nacionalidad.value}&genero=${genero}`;    
+}
+
+function enviar2(evento){
+    evento.preventDefault();
+    const mac = document.getElementById("nationality");
+    const genero = obtenerGenero();
+
+    const request = new Request(
+        `https://randomuser.me/api/?nat=${mac}&gender=${genero}`,
+        {
+            method: 'get',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }
+    );
+
+    fetch(request)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            localStorage.setItem("matchInfo",JSON.stringify(data.results[0]));
+            window.location.href= "its-a-match.html"
+        })
+        .catch(function (error) {
+            alert('Hubo un error encontrando a tu otra mitad :(');
+        });
 }
